cca646bb668ec1cfdde3151ae42d6881738e49da
Raehan <raehan900809@gmail.com>
2021-05-28 22:02:19 +0900

    Add login

--- 4/cookie2.js
+++ 4/cookie2.js
@@ -14,4 +14,19 @@ const parseCookies = (cookie = '') =>
 
 http.createServer(async (req, res) => {
   const cookies = parseCookies(req.headers.cookie)
+
+  if(req.url.startsWith('/login')) {
+    const query = url.parse(req.url);
+    const { name } = qs.parse(query);
+    const expires = new Date();
+
+    expires.setMinutes(expries.getMinutes() + 5);
+    res.writeHead(302, {
+      Location: '/',
+      'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`
+    });
+    res.end();
+  }
+}).listen(8084, () => {
+  console.log('on 8084')
 })

24cf7d8d8120256e4a361ce389cfce3f9d88e68e
Raehan <raehan900809@gmail.com>
2021-06-07 21:02:10 +0900

    Complete mongoDB

--- 8/mongo/app.js
+++ 8/mongo/app.js
@@ -4,9 +4,9 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
 const connect = require('./schemas');
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-// const commentsRouter = require('./routes/comments');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 
 const app = express();
 app.set('port', process.env.PORT || 3002);
@@ -22,9 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/comments', commentsRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);

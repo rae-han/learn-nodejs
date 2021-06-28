2605ee5efc4f2381887bfc6643ae0abf20f8f77a
Raehan <raehan900809@gmail.com>
2021-06-28 16:43:38 +0900

    Add socket io

--- 12gif-chat/socket.js
+++ 12gif-chat/socket.js
@@ -1,31 +1,58 @@
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
+const SocketIO = require('socket.io');
+
+const CONNECTION = 'connection';
+const DISCONNECT = 'disconnect';
+
+// module.exports = (server) => {
+//   const wss = new WebSocket.Server({ server });
+
+//   wss.on('connection', (ws, req) => {
+//     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//     console.log('새로운 클라이언트 접속', ip);
+
+//     ws.on('message', (message) => {
+//       console.log(message);
+//       console.log(new Date())
+//     })
+//     ws.on('error', (error) => {
+//       console.error(error);
+//     })
+//     ws.on('close', () => {
+//       console.log('클라이언트 접속 해제', ip);
+//       clearInterval(ws.interval)
+//     });
+
+//     ws.interval = setInterval(() => {
+//       if(ws.readyState === ws.OPEN) {
+//         ws.send(`서버에서 클라이언트로 메시지를 보냅니다.`)
+//       }
+//     }, 2000)
+
+//   })
+// }
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: '/socket.io' });
 
-const time = 0;
-
-
-  wss.on('connection', (ws, req) => {
+  io.on(CONNECTION, (socket) => { // 웹 소켓 연결 시
+    const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속', ip);
+    console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
 
-    ws.on('message', (message) => {
-      console.log(message);
+    socket.on(DISCONNECT, () => { // 연결 종료 시
+      console.log('클라이언트 접속 해제', ip, socket.id);
+      clearInterval(socket.interval);
     })
-    ws.on('error', (error) => {
+
+    socket.on('error', (error) => {
       console.error(error);
     })
-    ws.on('close', () => {
-      console.log('클라이언트 접속 해제', ip);
-      clearInterval(ws.interval)
-    });
-
-    ws.interval = setInterval(() => {
-      if(ws.readyState === ws.OPEN) {
-        ws.send(`서버에서 클라이언트로 메시지를 보냅니다.`)
-      }
-    }, 4000)
-
-  })
-}
+    socket.on('reply', (data) => {
+      console.log(data);
+    })
+    socket.interval = setInterval(() => {
+      socket.emit('news', 'Hello Socket.IO');
+    }, 3000)
+  });
+};

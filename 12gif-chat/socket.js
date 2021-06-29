3d5356dcdd7d1bcacd658ba0a5ccbff7a6e4542a
Raehan <raehan900809@gmail.com>
2021-06-29 20:13:56 +0900

    Add middleware for socket io

--- 12gif-chat/socket.js
+++ 12gif-chat/socket.js
@@ -1,5 +1,7 @@
 // const WebSocket = require('ws');
 const SocketIO = require('socket.io');
+// const { default: axios } = require('axios');
+const axios = require('axios');
 
 const CONNECTION = 'connection';
 const DISCONNECT = 'disconnect';
@@ -32,27 +34,85 @@ const DISCONNECT = 'disconnect';
 //   })
 // }
 
-module.exports = (server) => {
+// module.exports = (server) => {
+//   const io = SocketIO(server, { path: '/socket.io' });
+
+//   io.on(CONNECTION, (socket) => { // 웹 소켓 연결 시
+//     const req = socket.request;
+//     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//     console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+
+//     socket.on(DISCONNECT, () => { // 연결 종료 시
+//       console.log('클라이언트 접속 해제', ip, socket.id);
+//       clearInterval(socket.interval);
+//     })
+
+//     socket.on('error', (error) => {
+//       console.error(error);
+//     })
+//     socket.on('reply', (data) => {
+//       console.log(data);
+//     })
+//     socket.interval = setInterval(() => {
+//       socket.emit('news', 'Hello Socket.IO');
+//     }, 3000)
+//   });
+// };
+
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: '/socket.io' });
 
-  io.on(CONNECTION, (socket) => { // 웹 소켓 연결 시
+  app.set('io', io);
+
+  const room = io.of('/room');
+  const chat = io.of('/chat');
+
+  io.use((socket, next) => {
+    sessionMiddleware(socket.request, socket.request.res, next);
+  })
+
+  room.on(CONNECTION, (socket) => {
+    console.log('room 네임스페이스에 접속');
+
+    socket.on(DISCONNECT, () => {
+      console.log('room 네임스페이스 접속 해제');
+    });
+  });
+
+  chat.on(CONNECTION, (socket) => {
+    console.log('chat 네임스페이스에 접속');
     const req = socket.request;
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+    const { headers: { referer } } = req;
+    const roomId = referer
+      .split('/')[referer.split('/').length -1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
 
-    socket.on(DISCONNECT, () => { // 연결 종료 시
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
-    })
+    socket.to(roomId).emit('join', {
+      user: 'system',
+      chat: `${req.session.color}님이 입장.`
+    });
 
-    socket.on('error', (error) => {
-      console.error(error);
-    })
-    socket.on('reply', (data) => {
-      console.log(data);
-    })
-    socket.interval = setInterval(() => {
-      socket.emit('news', 'Hello Socket.IO');
-    }, 3000)
+    socket.on(DISCONNECT, () => {
+      console.log('chat 네임스페이스 접속 해제');
+      socket.leave(roomId)
+
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+      if(userCount === 0) {
+        axios.delete(`http://localhost:8005/room/${roomId}`)
+          .then(() => {
+            console.log('방 제거 요청 성공')
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color}님이 퇴장.`
+        })
+      }
+    });
   });
 };

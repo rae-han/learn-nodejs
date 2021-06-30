bbdfe4bbb929a3e44c8953d787b8218e8a0c53a7
Raehan <raehan900809@gmail.com>
2021-06-30 20:37:08 +0900

    Add chat

--- 12gif-chat/routes/index.js
+++ 12gif-chat/routes/index.js
@@ -50,10 +50,13 @@ router.get('/room/:id', async (req, res, next) => {
     if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
       return res.redirect('/?error=허용 인원이 초과하였습니다.');
     }
+
+    const chats = await Chat.find({ room: room._id }).sort('createAt');
+
     return res.render('chat', {
       room,
       title: room.title,
-      chats: [],
+      chats,
       user: req.session.color,
     });
   } catch (error) {
@@ -76,7 +79,23 @@ router.delete('/room/:id', async (req, res, next) => {
   }
 });
 
+router.post('/room/:id/chat', async (req, res, next) => {
+  try {
+    const chat = await Chat.create({
+      room: req.params.id,
+      user: req.session.color,
+      chat: req.body.chat,
+    });
+
+    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+    res.send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
 const test = require('../test');
-router.use('/test', test)
+router.use('/test', test);
 
 module.exports = router;

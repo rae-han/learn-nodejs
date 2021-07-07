076fac6a52ca8e8b2786f2551a915d415dbd8bb4
Raehan <raehan900809@gmail.com>
2021-07-07 16:14:44 +0900

    Add review cookie % session

--- 4cookiesession/cookie2.js
+++ 4cookiesession/cookie2.js
@@ -12,7 +12,10 @@ const parseCookies = (cookie = '') =>
       return acc;
     }, {});
 
+const session = {};
+
 http.createServer(async (req, res) => {
+  console.log(req.headers.cookie);
   const cookies = parseCookies(req.headers.cookie); // { mycookie: 'test' }
   // 주소가 /login으로 시작하는 경우
   if (req.url.startsWith('/login')) {
@@ -25,18 +28,27 @@ http.createServer(async (req, res) => {
     console.log('name', name)
     console.log('new Date()', new Date())
     const expires = new Date();
-    console.log('expires', expires)1
+    console.log('expires', expires)
     // 쿠키 유효 시간을 현재시간 + 5분으로 설정
-    expires.setMinutes(expires.getMinutes() + 5);
+    expires.setMinutes(expires.getMinutes() + 1);
+    console.log('modified expries', expires)
+    const uniqueInt = Date.now();
+    console.log(uniqueInt);
+    session[uniqueInt] = {
+      name,
+      expires
+    }
     res.writeHead(302, {
       Location: '/',
-      'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+      // 'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+      'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; trash=${encodeURIComponent('쓰레기')}; HttpOnly; Path=/`,
     });
     res.end();
   // name이라는 쿠키가 있는 경우
-  } else if (cookies.name) {
+  // } else if (cookies.name) {
+  } else if (cookies.session && session[cookies.session].expires > new Date()) {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end(`${cookies.name}님 안녕하세요`);
+    res.end(`${session[cookies.session].name}님 안녕하세요`);
   } else {
     try {
       const data = await fs.readFile('./cookie2.html');
@@ -51,4 +63,3 @@ http.createServer(async (req, res) => {
   .listen(8084, () => {
     console.log('8084번 포트에서 서버 대기 중입니다!');
   });
- 

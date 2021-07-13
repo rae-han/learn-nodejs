aa7b5caaecf7f91238e642b7b9fd4f692ec5b95d
Raehan <raehan900809@gmail.com>
2021-07-13 21:16:21 +0900

    Add socket io code review & retrial

--- 12gif-chat-4/socket.js
+++ 12gif-chat-4/socket.js
@@ -1,39 +1,71 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app) => {
   const io = SocketIO(server, { 
     path: '/socket.io',
   }); 
   // # option
   // - path: 클라이언트가 접속할 경로, 클라이언트에서도 이 경로와 일치하는 path를 넣어야 한다.
 
+  app.set('io', io);
+  // 라우터에서 io 객체를 쓸 수 있게 저장
 
-  io.on('connection', (socket) => {
-    const req = socket.request; 
-    // 요청 객체에 접근
-    // 응갑 객체는 socket.request.res 로 접근
-    // socket.id로 소켓 고유의 아이디를 가져올 수 있다.
-    // console.log('req: ', req);
-    // console.log(req.res);
-    console.log(socket.id)
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('req.headers["x-forwarded-for"], req.connection.remoteAddress', req.headers['x-forwarded-for'], req.connection.remoteAddress)
-    console.log('ip: ', ip)
+  const room = io.of('/room');
+  const chat = io.of('/chat');
+  // Socket.IO에 네임스페이스를 부여하는 메서드.
 
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스 접속');
+    socket.on('disconnect', () => {
+      console.log('room 네임스페이스 해제');
+    })
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스 접속');
+
+    const req = socket.request;
+    const { headers: { referer }} = req;
+    const roomId = referer
+      .split('/')[referer.split('/').length -1]
+      .replace(/\?.+/, '');
+    
+    socket.join(roomId);
 
     socket.on('disconnect', () => {
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
+      console.log('chat 네임스페이스 접속 해제');
+      socket.leave(roomId)
     })
 
-    socket.on('reply', (data) => { // reply는 사용자가 직접 만든 이벤트.
-      console.log(data);
-      console.log(new Date().getTime());
-    })
-
-    socket.interval = setInterval(() => {
-      socket.emit('news', 'Hello Socket.IO')
-    }, 4000)
   })
+
+  // io.on('connection', (socket) => {
+    
+  //   const req = socket.request; 
+  //   // 요청 객체에 접근
+  //   // 응갑 객체는 socket.request.res 로 접근
+  //   // socket.id로 소켓 고유의 아이디를 가져올 수 있다.
+  //   // console.log('req: ', req);
+  //   // console.log(req.res);
+  //   console.log(socket.id)
+  //   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  //   console.log('req.headers["x-forwarded-for"], req.connection.remoteAddress', req.headers['x-forwarded-for'], req.connection.remoteAddress)
+  //   console.log('ip: ', ip)
+
+  //   console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+
+  //   socket.on('disconnect', () => {
+  //     console.log('클라이언트 접속 해제', ip, socket.id);
+  //     clearInterval(socket.interval);
+  //   })
+
+  //   socket.on('reply', (data) => { // reply는 사용자가 직접 만든 이벤트.
+  //     console.log(data);
+  //     console.log(new Date().getTime());
+  //   })
+
+  //   socket.interval = setInterval(() => {
+  //     socket.emit('news', 'Hello Socket.IO')
+  //   }, 4000)
+  // })
 };

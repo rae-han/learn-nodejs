d4e330774d96466daa404e6b687745b78fc2bf9d
Raehan <raehan900809@gmail.com>
2021-07-15 14:38:47 +0900

    Add socket io review

--- 12gif-chat-4/app.js
+++ 12gif-chat-4/app.js
@@ -23,12 +23,7 @@ nunjucks.configure('views', {
 
 connect();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({ // app과 socket에 session을 공유하기 위해 변수로 분리
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -36,7 +31,14 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+})
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
 
 app.use((req, res, next) => {
   if(!req.session.color) {
@@ -65,4 +67,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app);
+webSocket(server, app, sessionMiddleware);
--- 12gif-chat-4/socket.js
+++ 12gif-chat-4/socket.js
@@ -1,6 +1,8 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { 
     path: '/socket.io',
   }); 
@@ -13,6 +15,13 @@ module.exports = (server, app) => {
   const room = io.of('/room');
   const chat = io.of('/chat');
   // Socket.IO에 네임스페이스를 부여하는 메서드.
+
+  io.use((socket, next) => { 
+    cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
+    // io.use 메서드에 미들웨어 장착
+    // 모든 웹 소켓 연결 시마다 실행
+    sessionMiddleware(socket.request, socket.request.res, next);
+  })
 
   room.on('connection', (socket) => {
     console.log('room 네임스페이스 접속');
@@ -32,9 +41,33 @@ module.exports = (server, app) => {
     
     socket.join(roomId);
 
+    socket.to(roomId).emit('join', {
+      user: 'system',
+      chat: `${req.session.color}님이 입장`
+    })
+
     socket.on('disconnect', () => {
       console.log('chat 네임스페이스 접속 해제');
+      
       socket.leave(roomId)
+
+      socket.leave(roomId);
+      const currentRoom =socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+      if(userCount === 0) { // 접속자가 0명이면 방 삭제
+        axios.delete(`http://localhost:8005/room/${roomId}`)
+          .then(() => {
+            console.log('방 제거 요청 성공')
+          })
+          .catch((error) => {
+            console.error(error)
+          });
+      } else {
+        socket.to(roomId).emit('exit', {
+          user: 'system',
+          chat: `${req.session.color}님이 퇴장`
+        })
+      }
     })
 
   })
@@ -69,3 +102,7 @@ module.exports = (server, app) => {
   //   }, 4000)
   // })
 };
+
+// 130 36 25820
+// 210 24 22330
+// 210 36 31770

6f6bf0fc859ec477b3188ebd99fb2dd1bc116e50
Raehan <raehan900809@gmail.com>
2021-07-27 16:04:16 +0900

    Init node-auction

--- 12gif-chat/websocket.js
+++ 12gif-chat/websocket.js
@@ -44,7 +44,7 @@ const DISCONNECT = 'disconnect';
 //     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 //     console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
 
-//     socket.on(DISCONNECT, () => { // 연결 종료 시
+//     socket.on(DISCONNECT, () => { // 연결 종료 시, 
 //       console.log('클라이언트 접속 해제', ip, socket.id);
 //       clearInterval(socket.interval);
 //     })

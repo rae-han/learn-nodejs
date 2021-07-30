9142e2a1742c6f9952383d434e88f2becdad0210
Raehan <raehan900809@gmail.com>
2021-07-30 20:12:53 +0900

    Fix error

--- 9sns-second/app.js
+++ 9sns-second/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
-const nunjecks = require('nunjucks');
+const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -12,7 +12,7 @@ const pageRouter = require('./routes/page');
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
-nunjecks.configure('views', {
+nunjucks.configure('views', {
   express: app,
   watch: true,
 })
@@ -45,9 +45,27 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  res.render('error')
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 })
 
-app.listen(app.get('port', () => {
-  console.log(app.get('port'), ' 번 포트에서 대기 중')
-}))
+// app.listen(app.get('port', () => {
+//   console.log(app.get('port'), ' 번 포트에서 대기 중')
+// }))
+// /Users/raehan/Documents/develop/learn/nodejs/node-textbook/9sns-second/node_modules/express/lib/router/index.js:139
+//   debug('dispatching %s %s', req.method, req.url);
+//                                  ^
+
+// TypeError: Cannot read property 'method' of undefined
+//     at Function.handle (/Users/raehan/Documents/develop/learn/nodejs/node-textbook/9sns-second/node_modules/express/lib/router/index.js:139:34)
+//     at Function.handle (/Users/raehan/Documents/develop/learn/nodejs/node-textbook/9sns-second/node_modules/express/lib/application.js:174:10)
+//     at Server.app (/Users/raehan/Documents/develop/learn/nodejs/node-textbook/9sns-second/node_modules/express/lib/express.js:39:9)
+//     at Object.onceWrapper (events.js:421:28)
+//     at Server.emit (events.js:315:20)
+//     at emitListeningNT (net.js:1352:10)
+//     at processTicksAndRejections (internal/process/task_queues.js:79:21)
+// [nodemon] app crashed - waiting for file changes before starting...
+
--- 9sns-second/routes/page.js
+++ 9sns-second/routes/page.js
@@ -11,11 +11,11 @@ router.use((req, res, next) => {
 });
 
 router.get('/profile', (req, res) => {
-  res.render('profile', { title: '내 정보 - NordBird' });
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
 router.get('/join', (req, res) => {
-  res.render('join', { title: '회원가입 - NodeBird' })
+  res.render('join', { title: '회원가입 - NodeBird' });
 });
 
 router.get('/', (req, res, next) => {
@@ -23,8 +23,7 @@ router.get('/', (req, res, next) => {
   res.render('main', {
     title: 'NodeBird',
     twits,
-  })
-})
-
+  });
+});
 
 module.exports = router;

42c8e05d6c3ca812cb3908c4aefc01a31f34e2fa
Raehan <raehan900809@gmail.com>
2021-08-02 22:17:26 +0900

    Add cookie-session

--- 9sns-second/app.js
+++ 9sns-second/app.js
@@ -46,7 +46,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  console.log(res.locals.error)
   res.render('error');
 });
 

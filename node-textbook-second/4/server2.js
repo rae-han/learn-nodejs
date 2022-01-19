1b21e582414bad85c4153258b996da38a603b2d2
Raehan <raehan900809@gmail.com>
2022-01-19 20:30:31 +0900

    add: simple noe server

---
+++ node-textbook-second/4/server2.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('hello node')
+  res.end('hello server')
+})
+
+server.listen(8080, () => {
+  console.log('in port 8080');
+});
+
+server.on('listening', () => {
+  console.log('In port 8080')
+})

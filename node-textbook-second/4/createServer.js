3c78cd21e7c7a8a8453f79c07f90995167a642d3
raehan <raehan900809@gmail.com>
2022-01-30 23:59:15 +0900

    add: send end

--- node-textbook-second/4/createServer.js
+++ node-textbook-second/4/createServer.js
@@ -4,8 +4,8 @@ const server = http.createServer((req, res) => {
   console.log('create server');
 
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.write('<h1>Hello Node!!</h1>');
-  // res.end('');
+  // res.write('<h1>Hello Node!!</h1>');
+  res.end('<p>Hello Server!!</p>');
 })
 
 server.listen(8080, () => {

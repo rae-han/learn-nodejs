abd0a69ae19229ccee686c5e59fe955bd50f620f
raehan <raehan900809@gmail.com>
2022-02-05 21:05:53 +0900

    add: deploy html in server

--- node-textbook-second/6/1/app.js
+++ node-textbook-second/6/1/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express');
+  // res.send('Hello, Express');
+  console.log(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(app.get('port'), () => {

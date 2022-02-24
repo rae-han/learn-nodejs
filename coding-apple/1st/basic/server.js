b641c58b538099ac9acb8575be750917fa678e92
Raehan <raehan900809@gmail.com>
2022-02-24 20:34:44 +0900

    add: mongo db connection

--- coding-apple/1st/basic/server.js
+++ coding-apple/1st/basic/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
+
+const mongoUrl = 'mongodb+srv://raehan809:fogks0619@cluster0.axmgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 app.get('/pet', (req, res) => {
   // res.send('GET /pet');
@@ -19,9 +22,16 @@ app.get('/write', (req, res) => {
 app.post('/add', (req, res) => {
   console.log(req.body);
   res.send('전송완료')
-
-})
-
-app.listen(8080, () => {
-  console.log('on 8080 port');
 });
+
+MongoClient.connect(mongoUrl, (err, client) => {
+  if(err) return console.log(err);
+
+  app.listen(8080, () => {
+    console.log('on 8080 port');
+  });
+});
+
+// app.listen(8080, () => {
+//   console.log('on 8080 port');
+// });

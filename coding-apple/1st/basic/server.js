aaa1c5d3071579f2e1b174bcdd9e64664034af30
Raehan <raehan900809@gmail.com>
2022-02-24 16:00:03 +0900

    add: route

--- coding-apple/1st/basic/server.js
+++ coding-apple/1st/basic/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
 app.get('/pet', (req, res) => {
   // res.send('GET /pet');
@@ -8,6 +10,16 @@ app.get('/pet', (req, res) => {
 
 app.get('/beauty', (req, res) => {
   res.send('GET /beauty')
+})
+
+app.get('/write', (req, res) => {
+  res.sendFile(__dirname+'/views/write.html');
+})
+
+app.post('/add', (req, res) => {
+  console.log(req.body);
+  res.send('전송완료')
+
 })
 
 app.listen(8080, () => {

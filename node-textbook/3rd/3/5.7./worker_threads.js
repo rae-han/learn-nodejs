1ddaef0d2e1690fb9e3ebb3bdbb757c388703b3b
Raehan <raehan900809@gmail.com>
2022-05-19 20:53:46 +0900

    add: worker test

--- node-textbook/3rd/3/5.7./worker_threads.js
+++ node-textbook/3rd/3/5.7./worker_threads.js
@@ -1,15 +1,23 @@
 const { Worker, isMainThread, parentPort } = require('worker_threads');
 
 if(isMainThread) { // 부모일 때
+  console.log('1. parent start')
   const worker = new Worker(__filename);
-  // console.log(worker)
-  worker.on('message', message => console.log('from worker', message));
-  worker.on('exit', () => console.log('worker exit'));
-  worker.postMessage('ping');
+  worker.on('message', message => console.log('2. from worker', message));
+  // worker.on('exit', () => console.log('3. worker exit'));
+  // worker.postMessage('ping')
+  // worker.postMessage('ping')
+  // worker.postMessage('ping')
+  // worker.postMessage('ping')
+
+  let interval = setInterval(() => worker.postMessage('ping'), 1000)
 } else {
+  console.log('2. children start')
   parentPort.on('message', value => {
-    console.log('from parent', value);
+    console.log('1. from parent', value);
     parentPort.postMessage('pong');
     // parentPort.close();
   })
+  console.log('3. children end')
 }
+
